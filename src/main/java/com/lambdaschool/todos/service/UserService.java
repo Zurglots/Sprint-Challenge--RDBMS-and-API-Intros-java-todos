361a58614098d5b26ea79d20388c462bf487095d
Zurglots <bfeole@gmail.com>
2019-07-19 11:18:04 -0400

    Added inital user system

---
+++ src/main/java/com/lambdaschool/todos/service/UserService.java
@@ -0,0 +1,21 @@
+package com.lambdaschool.todos.service;
+
+import com.lambdaschool.authenticatedusers.model.User;
+import com.lambdaschool.todos.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> findAll();
+
+    User findUserByName(String name);
+
+    User findUserById(long id);
+
+    void delete(long id);
+
+    User save(User user);
+
+    User update(User user, long id);
+}

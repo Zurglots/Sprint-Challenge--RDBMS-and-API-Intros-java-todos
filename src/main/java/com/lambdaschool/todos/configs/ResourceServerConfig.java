396d4b218341c51d5e3e952025354a34be9a476a
Zurglots <bfeole@gmail.com>
2019-07-19 20:10:25 -0400

    Controller update

--- src/main/java/com/lambdaschool/todos/configs/ResourceServerConfig.java
+++ src/main/java/com/lambdaschool/todos/configs/ResourceServerConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 
+import java.util.ArrayList;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter
--- src/main/java/com/lambdaschool/todos/controller/UserController.java
+++ src/main/java/com/lambdaschool/todos/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -77,6 +78,20 @@ public class UserController
         responseHeaders.setLocation(newUserURI);
 
         return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/todos/{todoid}",
+            consumes = {"application/json"})
+    public ResponseEntity<?> updateTodo(
+            @RequestBody
+                    Todo updateTodo,
+            @PathVariable
+                    long todoid)
+    {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        updateTodo.setUser(userService.findUserByName(authentication.getName()));
+        todoService.update(updateTodo, todoid);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
